7c4746450ecce40fade2eeb336a19e5a5164b631
jaipe5 <117586552+jaipe5@users.noreply.github.com>
2024-09-08 23:23:51 +0200

    First commit

---
+++ areaCuadrado/calcularCuadrado.js
@@ -0,0 +1,7 @@
+//Esta función calcula el área de un cuadrado
+function calcularCuadrado(lado) {
+    return lado * lado;
+}
+
+//Ejemplo de uso
+console.log(calcularCuadrado(5)); //25
---
+++ areaTriangulo/calcularTriangulo.js
@@ -0,0 +1,6 @@
+//Esta función halla el área de un triángulo
+function calcularTriangulo(base, altura) {
+    return (base * altura) / 2;
+}
+//Ejemplo de uso
+console.log(calcularTriangulo(5, 4)); //10
